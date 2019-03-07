845fdf9bc81b6457c81ba2a12de560618cdf2fc5
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-07 20:30:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 require_relative './spec_helper.rb'
 
- define "fizzbuzz".to i
-  if i % 3 == i % 5 == 0
+ define "fizzbuzz".to int
+  if int % 3 == int % 5 == 0
     "fizzbuzz"
   elsif
-    i % 3 == 0
+    int % 3 == 0
     "fizz"
   elsif
-    i % 5 == 0
+    int % 5 == 0
     "buzz"
 
    end
