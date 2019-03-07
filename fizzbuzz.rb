45aa71d8144c61177bfcf14470a6818813de3fb4
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-07 21:15:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 
 def fizzbuzz(int)
   	  if int % 3 == 0
-    "Fizz"
+    "FizzBuzz"
     elsif int % 5 == 0
     "Buzz"
     elsif int % 3 && 5
     "FizzBuzz"
-     "nil"
+     nil
     end
 end
 
