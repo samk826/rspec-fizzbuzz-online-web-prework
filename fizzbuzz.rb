b526bc6d2e2d2f57ce645f612d054416e09d1a15
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-07 20:20:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,19 @@
-def fizzbuzz
+define "fizzbuzz".to i
+  if i % 3 == i % 5 == 0
+    "fizzbuzz"
+  elsif
+    i % 3 == 0
+    "fizz"
+  elsif
+    i % 5 == 0
+    "buzz"
 
-end# Don't forget! This file needs to be 'required' in its spec file
+   end
+
+
+
+
+
+
+# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
