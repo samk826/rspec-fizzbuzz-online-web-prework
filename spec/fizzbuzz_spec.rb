4990a007e4abc2f5a4c4123aedda9aa641729c7e
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-07 20:17:34 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -21,20 +21,15 @@ describe "fizzbuzz" do
     fizz_4 = fizzbuzz(4)
 
     expect(fizz_4).to eq(nil)
-  end
-end
-
-ef fizz_buzz?(int)
-  it 'returns "Fizz" when the number is divisible by 3' do	  if int % 3 == 0
-    fizz_3 = fizzbuzz(3)	    "fizz"
-    elsif int % 5 == 0
-    "buzz"
-    elsif int % 3 && 5 || 3 && 5
-    "fizzbuzz"
-    else int !% 3 || 5
 
 
-     expect(fizz_3).to eq("Fizz")
-  end	  end
-  it 'returns "Buzz" when the number is divisible by 5' do
-     fizz_5 = fizzbuzz(5)	end
+    def fizz_buzz?(int)
+      it 'returns "Fizz" when the number is divisible by 3' do	  if int % 3 == 0
+        fizz_3 = fizzbuzz(3)	    "fizz"
+        elsif int % 5 == 0
+        "buzz"
+        elsif int % 3 && 5 || 3 && 5
+        "fizzbuzz"
+        else int !% 3 || 5
+        end
+      end
